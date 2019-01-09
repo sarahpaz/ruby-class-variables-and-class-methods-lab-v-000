d071bdf9ba4cb50168af12e40b86704c2789720f
Sarah Paz <sarah.paz@gmail.com>
2019-01-09 03:42:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
 
   def self.genre_count
     genre_count = {}
-    @@genres.each {|genre| genre_count[genre] += 1}
+    @@genres.each {|genre| genre_count[genre] += 1 if genre_count[genre]}
     genre_count
   end
 

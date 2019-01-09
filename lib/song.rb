07f1b9bd1fe00893d61923e5a3e0b5e10dd5a110
Sarah Paz <sarah.paz@gmail.com>
2019-01-09 03:15:26 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,41 @@
+class Song
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  attr_accessor :name, :song, :genre
+
+  def  initialize(name, song, genre)
+    @@count += 1
+    @@genres
+    @@artists
+    @name
+    @song
+    @genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres ||= []
+  end
+
+  def self.artists
+    @@artists ||= []
+  end
+
+end
+
+# class Album
+#   @@album_count = 0
+#
+#   def initialize
+#     @@album_count += 1
+#   end
+#
+#   def self.count
+#     @@album_count
+#   end
+# end

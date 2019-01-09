7a986eadb6b5d67f66b7f6e7b07c5f4d75083912
Sarah Paz <sarah.paz@gmail.com>
2019-01-09 03:38:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,14 +27,14 @@ class Song
   end
 
   def self.genre_count
-    genre_count = Hash.new {0}
+    genre_count = Hash.new{0}
     @@genres.each {|genre| genre_count[genre] += 1}
     genre_count
   end
-  #
-  # # def self.artist_count
-  # #   artist_count = {0}
-  # #   @@artists.each {|artist| artist_count[artist] +=1}
-  # #   artist_count
-  # # end
+
+  def self.artist_count
+    artist_count = Hash.new{0}
+    @@artists.each {|artist| artist_count[artist] +=1}
+    artist_count
+  end
 end
